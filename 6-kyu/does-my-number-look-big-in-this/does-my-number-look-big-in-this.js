94829dca2b4cb4b01897e385afc2ede52334e38a
Noel Odero <136817145+noel-odero@users.noreply.github.com>
2025-03-07 11:30:37 +0200

    Add solution - CodeHub

---
+++ 6-kyu/does-my-number-look-big-in-this/does-my-number-look-big-in-this.js
@@ -0,0 +1,11 @@
+function narcissistic(value) {
+    let newArr = value.toString().split('')
+    let pow = newArr.length
+    let total = 0;
+    for (let num of newArr){
+        total += Number(num) ** pow
+    }
+​
+    return total == value
+  }
+​
