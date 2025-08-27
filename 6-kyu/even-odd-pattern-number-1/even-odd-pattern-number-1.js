877dcbc2e4f04205ff17f4ad01db5c33f1cf055b
Noel Odero <136817145+noel-odero@users.noreply.github.com>
2025-08-27 10:08:09 +0200

    Update solution - CodeHub

--- 6-kyu/even-odd-pattern-number-1/even-odd-pattern-number-1.js
+++ 6-kyu/even-odd-pattern-number-1/even-odd-pattern-number-1.js
@@ -1,14 +1,4 @@
 function EvenOdd(arr){
-  if (arr.length == 0 ) return
-  
-  let number = arr[0];
-  for(let i = 1; i < arr.length; i++){
-    if(i % 2 == 1){
-      number *= arr[i]
-    } else{
-      number += arr[i]
-    }
-  }
-  return number
+   return arr.reduce((acc, curr, i) => i % 2 == 1 ? acc * curr: acc + curr )
 ​
 }
